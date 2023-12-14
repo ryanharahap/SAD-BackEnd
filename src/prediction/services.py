264f3e59af4f062d830768e93b5751d09f10303d
Jason Kanggara <jk.jasonkanggara@gmail.com>
2023-12-14 23:16:08 +0700

    chore: model update

--- src/prediction/services.py
+++ src/prediction/services.py
@@ -16,8 +16,9 @@ from wordcloud import WordCloud
 
 current_directory = os.path.dirname(os.path.realpath(__file__))
 playstore_model_path = os.path.join(current_directory, 'ml-model', 'model_playstore_v5.h5')
-youtube_model_path = os.path.join(current_directory, 'ml-model', 'model_youtube_v2.h5')
-news_model_path = os.path.join(current_directory, 'ml-model', 'model_news_v2.h5')
+youtube_model_path = os.path.join(current_directory, 'ml-model', 'model_youtube_v7.h5')
+news_model_path = os.path.join(current_directory, 'ml-model', 'model_news_v5.h5')
+kamus_alay_path = os.path.join(current_directory, 'csv', 'new_kamusalay.csv')
 
 # Load the model from the .pkl file
 # with open(news_model_path, 'rb') as file:
@@ -30,9 +31,13 @@ class PredictionService:
     self.playstore_model = load_model(playstore_model_path, compile=False)
     self.youtube_model = load_model(youtube_model_path, compile=False)
     self.news_model = load_model(news_model_path, compile=False)
-    self.tokenizaer = Tokenizer()
+    self.youtube_tokenizer = Tokenizer(num_words=5000, oov_token="<OOV>")
     self.playstore_tokenizer = Tokenizer(num_words=20000, oov_token="<OOV>")
+    self.news_tokenizer = Tokenizer(num_words=10000, oov_token="<OOV>")
     self.base_url = base_url
+    
+    alay_dict = pd.read_csv(kamus_alay_path, names=['original', 'replacement'], encoding='latin-1')
+    self.alay_dict_map = dict(zip(alay_dict['original'], alay_dict['replacement']))
   
   def playstore_predict(self, data: List[PlaystoreRequest]):
     result = []
@@ -45,8 +50,6 @@ class PredictionService:
       sequences = self.playstore_tokenizer.texts_to_sequences(datum.review)
       padded = pad_sequences(sequences, padding='post', maxlen=100, truncating='post')
       prediction = self.playstore_model.predict(padded)
-      # average_prediction = np.mean(prediction)
-      # print(average_prediction)
       pred_label = 1 if prediction[0][0] > 0.5 else 0
       sentiment = "Positive" if pred_label == 1 else "Negative"
 
@@ -84,14 +87,12 @@ class PredictionService:
     negative_count = 0
 
     for datum in data:
-      # Convert to a sequence
-      self.tokenizaer.fit_on_texts(datum.comment)
-      sequences = self.tokenizaer.texts_to_sequences([datum.comment])
-      # Pad the sequence
-      padded = pad_sequences(sequences, padding='post', maxlen=188)
+      self.youtube_tokenizer.fit_on_texts(datum.comment)
+      sequences = self.youtube_tokenizer.texts_to_sequences([datum.comment])
+      padded = pad_sequences(sequences, padding='post', maxlen=50)
       predictions = self.youtube_model.predict(padded)
-      print(predictions)
-      sentiment = "Negative" if predictions[0][0] >= 0.5 else "Positive"
+      pred_label = 1 if predictions[0][0] >= 0.5 else 0
+      sentiment = "Positive" if pred_label >= 0.5 else "Negative"
 
       if sentiment == 'Positive':
         positive_count += 1
@@ -128,16 +129,16 @@ class PredictionService:
     
     for datum in data:
       text_to_predict = self.__remove_symbols(datum.title)
+      text_to_predict = self.__normalize_alay(text_to_predict, self.alay_dict_map)
       tokenized_text = self.__tokenized(pd.Series([text_to_predict]))
       filtered_text = self.__remove_stopwords(tokenized_text.iloc[0])
 
-      text_seq = self.tokenizaer.texts_to_sequences([filtered_text])
-
-      text_pad = pad_sequences(text_seq, maxlen=22, padding='post')
-
-      pred_label = self.news_model.predict(text_pad)
-
-      sentiment = "Positive" if pred_label > 0.5 else "Negative"
+      self.news_tokenizer.fit_on_texts([filtered_text])
+      text_seq = self.news_tokenizer.texts_to_sequences([filtered_text])
+      text_pad = pad_sequences(text_seq, maxlen=22, padding='post', truncating='post')
+      predictions = self.news_model.predict(text_pad)
+      pred_label = 1 if predictions[0][0] > 0.5 else 0
+      sentiment = "Positive" if pred_label == 1 else "Negative"
 
       if sentiment == 'Positive':
         positive_count += 1
